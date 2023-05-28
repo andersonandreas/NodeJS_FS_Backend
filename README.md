fecdab982c834d57f5eed8b471102b14d85ddd88
Andreas A <adde1990@live.se>
2023-05-28 21:23:21 +0200

    minor change to readme

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # NodeJS_FS_Backend
 
-## To run the backend
+## To run the backend tesing
 
 ```shell
 npm i
 npm start
 ```
 
-## To test the backend
+## To test the backend testing
 
 ```shell
 npm i
--- server.js
+++ server.js
@@ -5,7 +5,3 @@ require('dotenv').config();
 http.createServer(app).listen(process.env.port, () => {
   console.log(`server is runnin on port: ${process.env.port}`);
 });
-
-let i = 0;
-
-console.log(hej);
